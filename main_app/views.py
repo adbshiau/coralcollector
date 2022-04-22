918bfc46a0e49db65f4cc9b1cb46e402a90a4c22
ayashiau-ga <angelicashiau@gmail.com>
2022-04-22 15:42:03 -0700

    index view renders

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# THIS IS JUST MOCKING SOME  DATA SINCE WE DONT HAVE MODELS YET
+# When we have a model, the class Coral and corals list will be deleted
+class Coral:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+corals = [
+  Coral('Nobu', 'tabby', 'foul little demon', 3),
+  Coral('Sumi', 'tortoise shell', 'diluted tortoise shell', 0),
+  Coral('Biri', 'black tripod', '3 legged cat', 4)
+]
+
 # Create your views here.
 def home(request):
     return HttpResponse('<h1>Hello!</h1>')
 
 def about(request):
     return render(request, 'about.html')
+
+def corals_index(request):
+    return render(request, 'corals/index.html', {'corals': corals})
--- thefragtank/asgi.py
+++ thefragtank/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for coralcollector project.
+ASGI config for thefragtank project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coralcollector.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thefragtank.settings')
 
 application = get_asgi_application()
