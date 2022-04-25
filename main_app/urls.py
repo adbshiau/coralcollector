6f43547135c64f33811018ec808f2ee8869b683e
ayashiau-ga <angelicashiau@gmail.com>
2022-04-25 16:56:47 -0700

    css updated

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('corals/',views.corals_index, name='index'),
+    path('corals/', views.corals_index, name='index'),
     path('corals/<int:coral_id>/', views.corals_detail, name='detail'),
 ]
