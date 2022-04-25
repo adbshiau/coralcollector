f811bb083c67bde458e34fc9351d7d3d344ae3fb
ayashiau-ga <angelicashiau@gmail.com>
2022-04-25 13:30:50 -0700

    Coral model created

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Coral(models.Model):
+    trade_name = models.CharField(max_length=100)
+    scientific_name = models.CharField(max_length=100)
+    coral_type = models.CharField(max_length=50)
+    difficulty = models.CharField(max_length=50)
+    lighting = models.CharField(max_length=50)
+    water_flow = models.CharField(max_length=50)
+    notes = models.CharField(max_length=250)
+
