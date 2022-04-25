a86b84a22a1258b6cbd59f8948a2a193657f85c3
ayashiau-ga <angelicashiau@gmail.com>
2022-04-25 13:52:33 -0700

    index view updated for coral properties

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Coral
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Hello!</h1>')
+  return HttpResponse('<h1>Hello!</h1>')
 
 def about(request):
-    return render(request, 'about.html')
+  return render(request, 'about.html')
 
 def corals_index(request):
-    return render(request, 'corals/index.html', {'corals': corals})
+  corals = Coral.objects.all()
+  return render(request, 'corals/index.html', {'corals': corals})
