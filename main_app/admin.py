e92e402855aa8114e0a56b916b3b64a1bb97a3f9
ayashiau-ga <angelicashiau@gmail.com>
2022-04-27 15:43:11 -0700

    detail page updated to show coral locations

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Coral, Note
+from .models import Coral, Note, Location
 
 # Register your models here.
 admin.site.register(Coral)
 admin.site.register(Note)
+admin.site.register(Location)
 
