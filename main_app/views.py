36a4a1551f08583df170f9cececee9f45e8de4a6
ayashiau-ga <angelicashiau@gmail.com>
2022-04-27 05:57:57 -0700

    select dropdowns added

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from typing import List
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Coral
