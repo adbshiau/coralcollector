fccf4629fa62080c2ee2fcd6a3f521dc9bdae37a
ayashiau-ga <angelicashiau@gmail.com>
2022-04-27 16:46:07 -0700

    location CRUD applications added

--- main_app/urls.py
+++ main_app/urls.py
@@ -11,4 +11,9 @@ urlpatterns = [
     path('corals/<int:pk>/delete/', views.CoralDelete.as_view(), name='corals_delete'),
     path('corals/<int:coral_id>/add_note/', views.add_note, name='add_note'),
     path('corals/<int:coral_id>/assoc_location/<int:location_id>/', views.assoc_location, name='assoc_location'),
+    path('locations/', views.LocationList.as_view(), name='locations_index'),
+    path('locations/<int:pk>/', views.LocationDetail.as_view(), name='locations_detail'),
+    path('locations/create/', views.LocationCreate.as_view(), name='locations_create'),
+    path('locations/<int:pk>/update/', views.LocationUpdate.as_view(), name='locations_update'),
+    path('locations/<int:pk>/delete/', views.LocationDelete.as_view(), name='locations_delete'),
 ]
