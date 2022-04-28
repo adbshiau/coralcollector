db65017dee259fd03d9d5ad8c264b85b05113490
ayashiau-ga <angelicashiau@gmail.com>
2022-04-28 12:34:46 -0700

    photo appears on the page

--- main_app/models.py
+++ main_app/models.py
@@ -81,7 +81,7 @@ class Note(models.Model):
     
 class Photo(models.Model):
     url = models.CharField(max_length=200)
-    cat = models.ForeignKey(Coral, on_delete=models.CASCADE)
+    coral = models.ForeignKey(Coral, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Photo for cat_id: {self.coral_id} @{self.url}"
