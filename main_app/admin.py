4f655e1afe19c43899840201b5e68b0180c97106
ayashiau-ga <angelicashiau@gmail.com>
2022-04-28 12:23:23 -0700

    photo model added to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Coral, Note, Location
+from .models import Coral, Note, Location, Photo
 
 # Register your models here.
 admin.site.register(Coral)
 admin.site.register(Note)
 admin.site.register(Location)
+admin.site.register(Photo)
 
