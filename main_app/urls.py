de6f5eb5b29a379f83b1b2ec640e61340cc36f93
ayashiau-ga <angelicashiau@gmail.com>
2022-04-28 12:16:40 -0700

    add_photo function added

--- main_app/urls.py
+++ main_app/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('corals/<int:pk>/delete/', views.CoralDelete.as_view(), name='corals_delete'),
     path('corals/<int:coral_id>/add_note/', views.add_note, name='add_note'),
     path('corals/<int:coral_id>/assoc_location/<int:location_id>/', views.assoc_location, name='assoc_location'),
+    path('corals/<int:coral_id>/add_photo/', views.add_photo, name='add_photo'),
     path('locations/', views.LocationList.as_view(), name='locations_index'),
     path('locations/<int:pk>/', views.LocationDetail.as_view(), name='locations_detail'),
     path('locations/create/', views.LocationCreate.as_view(), name='locations_create'),
--- main_app/views.py
+++ main_app/views.py
@@ -3,8 +3,13 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
-from .models import Coral, Location
+from .models import Coral, Location, Photo
 from .forms import NoteForm
+import uuid
+import boto3
+
+S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
+BUCKET = 'the-frag-tank'
 
 class CoralCreate(CreateView):
   model = Coral
@@ -68,3 +73,21 @@ def add_note(request, coral_id):
 def assoc_location(request, coral_id, location_id):
   Coral.objects.get(id=coral_id).locations.add(location_id)
   return redirect('detail', coral_id=coral_id)
+
+def add_photo(request, coral_id):
+    # photo-file will be the "name" attribute on the <input type="file">
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        # need a unique "key" for S3 / needs image file extension too
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        # just in case something goes wrong
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            # build the full url string
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            # we can assign to cat_id or cat (if you have a cat object)
+            Photo.objects.create(url=url, coral_id=coral_id)
+        except:
+            print('An error occurred uploading file to S3')
+    return redirect('detail', coral_id=coral_id)
