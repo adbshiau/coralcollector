f9332ff43cab3987cef527d125e8ab54addca0d6
ayashiau-ga <angelicashiau@gmail.com>
2022-04-29 08:25:57 -0700

    authorization implemented

--- main_app/views.py
+++ main_app/views.py
@@ -5,6 +5,8 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Coral, Location, Photo
 from .forms import NoteForm
 import uuid
@@ -13,7 +15,7 @@ import boto3
 S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
 BUCKET = 'the-frag-tank'
 
-class CoralCreate(CreateView):
+class CoralCreate(LoginRequiredMixin, CreateView):
   model = Coral
   fields = ['trade_name', 'scientific_name', 'coral_type', 'difficulty', 'lighting', 'water_flow']
 
@@ -21,29 +23,29 @@ class CoralCreate(CreateView):
     form.instance.user = self.request.user
     return super().form_valid(form)
 
-class CoralUpdate(UpdateView):
+class CoralUpdate(LoginRequiredMixin, UpdateView):
   model = Coral
   fields = ['scientific_name', 'coral_type', 'difficulty', 'lighting', 'water_flow']
 
-class CoralDelete(DeleteView):
+class CoralDelete(LoginRequiredMixin, DeleteView):
   model = Coral
   success_url = '/corals/'
 
-class LocationList(ListView):
+class LocationList(LoginRequiredMixin, ListView):
   model = Location
 
-class LocationDetail(DetailView):
+class LocationDetail(LoginRequiredMixin, DetailView):
   model = Location
 
-class LocationCreate(CreateView):
-  model = Location
-  fields = '__all__'
-
-class LocationUpdate(UpdateView):
+class LocationCreate(LoginRequiredMixin, CreateView):
   model = Location
   fields = '__all__'
 
-class LocationDelete(DeleteView):
+class LocationUpdate(LoginRequiredMixin, UpdateView):
+  model = Location
+  fields = '__all__'
+
+class LocationDelete(LoginRequiredMixin, DeleteView):
   model = Location
   success_url = '/locations/'
 
@@ -54,10 +56,12 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def corals_index(request):
   corals = Coral.objects.filter(user=request.user)
   return render(request, 'corals/index.html', {'corals': corals})
 
+@login_required
 def corals_detail(request, coral_id):
   coral = Coral.objects.get(id=coral_id)
   unused_locations = Location.objects.exclude(id__in = coral.locations.all().values_list('id'))
@@ -67,6 +71,7 @@ def corals_detail(request, coral_id):
     'locations': unused_locations
   })
 
+@login_required
 def add_note(request, coral_id):
   form = NoteForm(request.POST)
   # validate the form
@@ -76,10 +81,12 @@ def add_note(request, coral_id):
     new_note.save()
   return redirect('detail', coral_id=coral_id)
 
+@login_required
 def assoc_location(request, coral_id, location_id):
   Coral.objects.get(id=coral_id).locations.add(location_id)
   return redirect('detail', coral_id=coral_id)
 
+@login_required
 def add_photo(request, coral_id):
     # photo-file will be the "name" attribute on the <input type="file">
     photo_file = request.FILES.get('photo-file', None)
